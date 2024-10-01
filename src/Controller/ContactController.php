a8dcbb981bc875091f877216c7e9505149699fc1
Lambda <tom.billet@ecoles-epsi.net>
2024-10-01 10:10:18 +0200

    controler Contact et page contacts-list

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ContactRepository;
+
+class ContactController extends AbstractController
+{
+
+    #[Route('/contacts-list', name: 'app_contacts_lists')]
+    public function ContactsList(ContactRepository $contactRepository): Response {
+        $contacts = $contactRepository->findAll();
+        return $this->render('contact/contacts_list.html.twig', [
+            'contacts' => $contacts
+        ]); 
+    }
+}
