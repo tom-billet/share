0e0dc91472b9db46dc6fb242a243ee2321558b74
Lambda <tom.billet@ecoles-epsi.net>
2024-10-15 09:35:19 +0200

    init collaboration

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -16,11 +16,11 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
-            ->add('topic', TextType::class, ['attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
-            ->add('email', EmailType::class, ['attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
-            ->add('message', TextareaType::class, ['attr' => ['class'=> 'form-control', 'rows'=>'7', 'cols'=>'7'], 'label_attr' => ['class'=>'fw-bold']])
-            ->add('send', SubmitType::class, ['attr' => ['class' => 'btn bg-primary text-white m4'], 'row_attr' => ['class'=>'text-center'],])
+            ->add('name', TextType::class, ['label' => 'Nom', 'attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
+            ->add('topic', TextType::class, ['label' => 'Sujet', 'attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
+            ->add('email', EmailType::class, ['label' => 'Adresse Mail', 'attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
+            ->add('message', TextareaType::class, ['label' => 'Message', 'attr' => ['class'=> 'form-control', 'rows'=>'7', 'cols'=>'7'], 'label_attr' => ['class'=>'fw-bold']])
+            ->add('send', SubmitType::class, ['label' => 'Envoyer', 'attr' => ['class' => 'btn bg-primary text-white m4'], 'row_attr' => ['class'=>'text-center'],])
         ;
     }
 
