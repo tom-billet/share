772e9868fdd1fbf8b844b4693e5901469d4b8fd5
Elise <elise.bonnet1@ecoles-epsi.net>
2024-11-02 15:53:56 +0100

    Tous les delete Category fonctionnent (partie 5 terminée)

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,23 +10,40 @@ use App\Entity\Category;
 use App\Form\EditCategoryType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use App\Form\DeleteCategoryType;
 
 class CategoryController extends AbstractController
 {
 
     #[Route('/moderation/categories', name: 'app_categories')]
-    public function Categories(CategoryRepository $categoryRepository): Response {
+    public function Categories(Request $request, CategoryRepository $categoryRepository, EntityManagerInterface $em): Response {
 
         $categories = $categoryRepository->findAll();
+        $form = $this->createForm(DeleteCategoryType::class, null, [
+            'categories' => $categories
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedCategories = $form->get('categories')->getData();
+            foreach ($selectedCategories as $category) {
+                $em->remove($category);
+            }
+            $em->flush();
+
+            $this->addFlash('notice', 'Catégories supprimées avec succès');
+            return $this->redirectToRoute('app_categories');
+        }
 
         return $this->render('category/categories.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'form' => $form->createView(),
         ]); 
     }
 
 
     #[Route('/moderation/edit-category/{id}', name: 'app_edit_category')]
-    public function EditCategory(Request $request, Category $category, EntityManagerInterface $em): Response {
+    public function editCategory(Request $request, Category $category, EntityManagerInterface $em): Response {
 
         $form = $this->createForm(EditCategoryType::class, $category);
         if($request->isMethod('POST')){
@@ -42,5 +59,18 @@ class CategoryController extends AbstractController
         return $this->render('category/edit_category.html.twig', [
             'form' => $form->createView()
         ]); 
+    }
+
+
+    #[Route('/moderation/delete-category/{id}', name: 'app_delete_category')]
+    public function deleteCategory(Request $request, Category $category, EntityManagerInterface $em): Response {
+
+        if($category!=null){
+            $em->remove($category);
+            $em->flush();
+            $this->addFlash('notice','Catégorie supprimée');
+        }
+
+        return $this->redirectToRoute('app_categories');
     }
 }
