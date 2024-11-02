14b772361b266dac554fbabb84acd6495ccdeb5f
Elise <elise.bonnet1@ecoles-epsi.net>
2024-11-02 19:58:27 +0100

    12.2 et 12.3 : l'utilisateur peut ajouter des fichiers dans son profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use App\Repository\UserRepository;
+use App\Repository\SubcategoryRepository;
+use App\Entity\File;
+use App\Form\FileUserType;
 
 class UserController extends AbstractController
 {
@@ -19,13 +25,56 @@ class UserController extends AbstractController
     }
 
     #[Route('/private/account', name: 'app_account')]
-    public function profil(UserRepository $userRepository): Response
+    public function profil(UserRepository $userRepository, Request $request, SubcategoryRepository $subcategoryRepository, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         
         $user = $this->getUser();
 
+        $file = new File();
+        $subcategories = $subcategoryRepository->findBy([], ['category'=>'asc', 'number'=>'asc']);
+        $form = $this->createForm(FileUserType::class, $file, ['subcategories'=>$subcategories]);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isSubmitted()&&$form->isValid()){
+                $file->setSendingDate(new \Datetime());
+                $selectedSubcategories = $form->get('subcategories')->getData();
+                foreach ($selectedSubcategories as $subcategory) {
+                    $file->addSubcategory($subcategory);
+                }
+
+                $f = $form->get('file')->getData();
+                if($f){
+                    $nomFichierServeur = pathinfo($f->getClientOriginalName(),PATHINFO_FILENAME);
+                    $nomFichierServeur = $slugger->slug($nomFichierServeur);
+                    $nomFichierServeur = $nomFichierServeur.'-'.uniqid().'.'.$f->guessExtension();
+                    try{
+                        $file->setServerName($nomFichierServeur);
+                        $file->setOriginalName($f->getClientOriginalName());
+                        $file->setSendingDate(new \Datetime());
+                        $file->setExtension($f->guessExtension());
+                        $file->setSize($f->getSize());
+                        $file->setUser($user);
+                        $em->persist($file);
+                        $em->flush();
+
+                        $f->move($this->getParameter('file_directory'), $nomFichierServeur);
+                        $this->addFlash('notice', 'Fichier envoyé');
+                        return $this->redirectToRoute('app_account');
+                    }
+                    catch(FileException $e){
+                        $this->addFlash('notice', 'Erreur d\'envoi');
+                    }
+                }
+            }
+        }
+
+
+
         return $this->render('user/account.html.twig', [
-            'user'=>$user
+            'user'=>$user,
+            'form' => $form->createView(),
+            'subcategories'=> $subcategories
         ]);
     }
 }
---
+++ src/Form/FileUserType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\File;
+use App\Entity\User;
+use App\Entity\Subcategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType as TypeFileType;
+use Symfony\Component\Validator\Constraints\File as ValidatorFile;
+
+class FileUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('file', TypeFileType::class, array('label' => 'Fichier', 'mapped'=>false,'attr' => ['class'=>'form-control'], 'label_attr' => ['class'=> 'fw-bold'],'constraints' => [
+            new ValidatorFile([
+                'maxSize' => '200k',
+                'mimeTypes' => [
+                'application/pdf',
+                'application/x-pdf',
+                'image/jpeg',
+                'image/png',
+                ],
+            'mimeTypesMessage' => 'Le site accepte uniquement les fichiers PDF, PNG et JPG',
+            ])
+        ],))
+            ->add('subcategories', EntityType::class, [
+                'class' => Subcategory::class,
+                'choices' => $options['subcategories'],
+                'choice_label' => 'label',
+                'expanded' => true,
+                'multiple' => true,
+                'label' => false, 'mapped' => false])
+            ->add('ajouter', SubmitType::class, ['attr' => ['class' => 'btn bg-primary text-white m4'], 'row_attr' => ['class'=>'text-center'],])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => File::class,
+            'subcategories' => []
+        ]);
+    }
+}
